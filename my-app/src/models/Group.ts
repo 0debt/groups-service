ab56c8d7ca212b0d79307c7435bd41d8b6fa4747
luigi <luigididonna89@gmail.com>
2025-11-24 20:47:27 +0100

    Funzione Research in group

--- my-app/src/models/Group.ts
+++ my-app/src/models/Group.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import { createApi } from "unsplash-js";
 
 export interface IGroup {
     name: string;
@@ -17,19 +17,65 @@ const groupSchema = new mongoose.Schema<IGroup>({
     imageUrl: { type: String }
 });
 
-export async function createGroup(name: string, description: string, members: string[], imageUrl: string) {
-
-    const group = new Group({
-        name,
-        description,
-        members,
-        imageUrl: 'https://example.com/image.png'
-    });
-
-    await group.save();
-    console.log('Group created:', group);
-    return group;
-}
 
 export const Group = mongoose.model<IGroup>('Group', groupSchema);
+
+
+export async function requestPhoto() {
+
+    const unsplash = createApi({
+        accessKey: "jmxmcCTz_JxIDWE9wkKCcWyy9TZ9UfCtF1Oza9xnpIc",
+        fetch,
+    });
+    const result = await unsplash.photos.getRandom({ count: 1 });
+
+    if (result.type === "success") {
+        const imageUrl = Array.isArray(result.response) ? result.response[0] : result.response // URL della foto da usare
+        const photo = imageUrl.urls.regular
+        console.log("URL FOTO:", photo);
+        return photo;
+    } else {
+        console.error("Errore nel recuperare la foto da Unsplash:", result.errors);
+        return "";
+    }
+
+
+}
+
+export async function createGroup(name: string, description: string, members: string[]) {
+
+    try {
+
+
+        const imageUrl = await requestPhoto();
+
+
+        const group = new Group({
+            name,
+            description,
+            members,
+            imageUrl,
+        });
+
+        await group.save();
+
+
+        return group;
+    } catch (error) {
+        console.error(" ERRORE IN CREAZIONE:", error);
+        throw error;
+    }
+}
+
+export async function ReserachchByName(memberName: string) {
+    try {
+        const groups = await Group.find({ members: memberName });
+        return groups;
+    } catch (error) {
+        console.error("ERRORE NELLA RICERCA:", error);
+        throw error;
+    }
+}
+
+
 
