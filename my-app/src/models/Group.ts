9640b147eabb6b5c334d89fd3b699097f7a49ef6
GiuliaBarresi <giulia.barresi@mail.polimi.it>
2025-11-25 15:40:00 +0100

    update e delete

--- my-app/src/models/Group.ts
+++ my-app/src/models/Group.ts
@@ -33,3 +33,50 @@ export async function createGroup(name: string, description: string, members: st
 
 export const Group = mongoose.model<IGroup>('Group', groupSchema);
 
+//delete = eliminare il gruppo
+export async function deleteGroup(groupId: string) {
+    const group = await Group.findById(groupId);
+    if (!group) {
+        throw new Error('Group not found');
+    }
+
+    await Group.deleteOne({ _id: groupId });
+    console.log('Group deleted:', group);
+    return group;
+}
+
+
+//update = aggiungere o eliminare un membro
+export async function updateGroupMembers( groupId: string, memberToAdd?: string, memberToRemove?: string){
+    const group = await Group.findById(groupId);
+    if (!group) {
+        throw new Error("Group not found");
+    }
+
+    let modified: boolean = false;
+
+    // Aggiungi un membro
+    if (memberToAdd && !group.members.includes(memberToAdd)) {
+        group.members.push(memberToAdd);
+        modified = true;
+    }
+
+    // Rimuovi un membro
+    if (memberToRemove && group.members.includes(memberToRemove)) {
+        group.members = group.members.filter(
+            (member: string) => member !== memberToRemove
+        );
+        modified = true;
+    }
+
+    // Se non è stato fatto nulla, non salvare
+    if (!modified) {
+        console.log("No modification has been done.");
+        return group;
+    }
+
+    // Salva solo se cambiato
+    await group.save();
+    console.log("Group updated:", group);
+    return group;
+}
